e9b0dc6ca7cdd77b187953c62621c303f7565c38
marhiru <marcosgamern@gmail.com>
2025-07-29 19:02:53 -0300

    feat: model req and res test using json and error treat

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,11 +2,21 @@ package controller
 
 import (
 	"api/src/configuration/rest_err"
+	"api/src/model/request"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Invalid request body")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("Invalid request body %s", err.Error()),
+		)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
