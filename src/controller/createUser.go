3435947842b5b9934cd8b7b2b0bba50e40232c42
marhiru <marcosgamern@gmail.com>
2025-07-29 00:55:32 -0300

    feat: testing rest_err returns

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"api/src/configuration/rest_err"
+
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
-
+	err := rest_err.NewBadRequestError("Invalid request body")
+	c.JSON(err.Code, err)
 }
