ef37505959f0540bee2116e51534a47a0c5c4b9a
marhiru <marcosgamern@gmail.com>
2025-07-29 21:31:32 -0300

    fix: removed comment

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -12,9 +12,6 @@ func CreateUser(c *gin.Context) {
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		// restErr := rest_err.NewBadRequestError(
-		// 	fmt.Sprintf("Invalid request body %s", err.Error()),
-		// )
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
